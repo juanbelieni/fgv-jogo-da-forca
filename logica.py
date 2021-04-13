2b0d7d696b70c4f94ddb817117f469a1671d6073
Juan Belieni <>
2021-04-13 19:52:59 +0000

    Finalização do módulo lógico

--- logica.py
+++ logica.py
@@ -2,3 +2,37 @@
 Módulo lógico do jogo da forca
 Grupo: Juan, Lucas, Yasmin, Nicole e Carlos
 """
+
+import random
+
+def esconder_palavra(palavra_secreta):
+  '''Retorna um string de underline de mesmo tamanho da palavra secreta'''
+  return("_" * len(palavra_secreta))
+
+def revelar_letras(palavra_escondida, palavra_secreta, tentativas, letra):
+  '''Retorna a palavra escondida com as letras reveladas'''
+  i = 0
+  acertos = 0
+  auxiliador = palavra_escondida
+  while  (i < len(palavra_secreta)):
+    if(palavra_secreta[i] == letra):
+      palavra_escondida = palavra_escondida[:i] + letra + palavra_escondida[i + 1:]
+      acertos += 1
+    i += 1
+  if(acertos > 0):
+    print(f"Parabéns! Você acertou {acertos} letra(s)!")
+    if(auxiliador == palavra_escondida):
+      print("Mas, pela falta de atenção na repetição das letras, perdeu uma chance S2")
+      tentativas -= 1
+      print(f"Agora você tem {tentativas} tentativas")
+  else:
+    print("Infelizmente, não houve acertos :(")
+    tentativas -= 1
+    print(f"Agora você tem {tentativas} chances")
+  return palavra_escondida
+
+def escolher_palavra():
+  palavras = open("palavras.txt", "r")
+  palavras = palavras.read().split('\n')
+  palavra_secreta = random.choice(palavras)
+  return palavra_secreta
--- main.py
+++ main.py
@@ -1,28 +1,50 @@
-# import logica
-# import grafico
+"""
+       __                         __         ______                     
+      / /___  ____ _____     ____/ /___ _   / ____/___  ______________ _
+ __  / / __ \/ __ `/ __ \   / __  / __ `/  / /_  / __ \/ ___/ ___/ __ `/
+/ /_/ / /_/ / /_/ / /_/ /  / /_/ / /_/ /  / __/ / /_/ / /  / /__/ /_/ / 
+\____/\____/\__, /\____/   \__,_/\__,_/  /_/    \____/_/   \___/\__,_/  
+           /____/     
+
+Grupo: Juan, Lucas, Yasmin, Nicole e Carlos
+"""
+
+from logica import escolher_palavra, revelar_letras, esconder_palavra
+
+jogar = int(input("Você quer jogar forca? Insira 1 para jogar ou 0 para sair: "))
+
+if(jogar == 1):
+  print("Você tem 6 chances para acertar!")
+
+else:
+  print("Então deixa para próxima! Até mais :)")
 
 
-# palavras = open("palavras.txt", "r")
-# print([palavra for palavra in palavras.read().split('\n') if len(palavra) > 5][:10])
+while(jogar == 1):
 
-#    _____                           __              __        ____                       
-#   / ___/___  ____ ___  ___  ____  / /____     ____/ /__     / __ )____ __________  ____ 
-#   \__ \/ _ \/ __ `__ \/ _ \/ __ \/ __/ _ \   / __  / _ \   / __  / __ `/ ___/ __ \/ __ \
-#  ___/ /  __/ / / / / /  __/ / / / /_/  __/  / /_/ /  __/  / /_/ / /_/ / /__/ /_/ / / / /
-# /____/\___/_/ /_/ /_/\___/_/ /_/\__/\___/   \__,_/\___/  /_____/\__,_/\___/\____/_/ /_/ 
-                                                                                        
-#          ,.
-#         (_|,.
-#        ,' /, )_____
-#     __j o``-'
-#    (")
-#     `-j
-#       `-._(
-#          |_\  |--^.
-#         /_]'|_| /_
-#            /_]'  /
+  palavra_secreta = escolher_palavra()
+  palavra_escondida = esconder_palavra(palavra_secreta)
 
-print("abacaxi".replace("a", ""))
+  tentativas = 6
 
+  while(tentativas != 0):
+   
+    letra = str(input("\nInsira uma letra: "))
 
-print("_" * 8)
+    auxiliador = palavra_escondida
+    palavra_escondida = revelar_letras(palavra_escondida, palavra_secreta, tentativas, letra)
+    print(palavra_escondida)
+
+    if(auxiliador == palavra_escondida):
+      tentativas -= 1
+
+    if "_" not in palavra_escondida:
+      print("\nVocê ganhou! \°/")
+      break;
+
+    if tentativas <= 0:
+      print(f"\nVocê perdeu! A palavra era {palavra_secreta}.")
+
+  jogar = int(input("\nDeseja jogar novamente? Insira 1 para jogar ou 0 para sair: "))
+  if(jogar == 0):
+    print("Então deixa para próxima! Até mais :)")
